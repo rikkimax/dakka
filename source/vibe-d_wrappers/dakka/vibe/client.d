a9e870af171eb8ae10666c496ee66b06691e5c56
rikki cattermole <pbdadmin@gmail.com>
2014-07-24 18:17:45 +1200

    Wrappers around Vibe's HTTPServerRequest, HTTPResponse, "Cookie" and Session.

---
+++ source/vibe-d_wrappers/dakka/vibe/client.d
@@ -0,0 +1,142 @@
+﻿module dakka.vibe.client;
+import dakka.vibe.server;
+import vibe.http.common : HTTPResponse, HTTPRequest, HTTPVersion, HTTPMethod;
+import vibe.http.server : HTTPServerRequest, SessionOption;
+
+class DakkaHTTPResponse : HTTPResponse {
+	private {
+		HTTPReqResp reqresp_;
+	}
+
+	this(HTTPReqResp reqresp) {
+		reqresp_ = reqresp;
+	}
+
+	void writeBody(ubyte[] data, string content_type = null) { reqresp_.response_writeBody(data, content_type); }
+	void writeVoidBody() { reqresp_.response_writeVoidBody(); }
+	void redirect(string url, int status = HTTPStatus.Found) { reqresp_.response_redirect(url, status); }
+	DakkaCookie setCookie(string name, string value, string path = "/") { return DakkaCookie(reqresp_, name, value, path); }
+	DakkaSession startSession(string path = "/", SessionOption options = SessionOption.httpOnly) { return DakkaSession(reqresp_, path, cast(size_t)options); }
+	void terminateSession() { reqresp_.response_terminateSession(); }
+}
+
+struct DakkaCookie {
+	private {
+		HTTPReqResp reqresp_;
+		string m_name;
+
+		string m_value;
+		string m_domain;
+		string m_path;
+		string m_expires;
+		long m_maxAge;
+	}
+
+	this(HTTPReqResp reqresp, string m_name, string value, string path) {
+		reqresp_ = reqresp;
+		this.m_name = m_name;
+		this.m_value = value;
+		this.m_path = path;
+		set();
+	}
+
+	@property string name() const { return m_name; }
+
+	@property void value(string value) { m_value = value; set(); }
+	@property string value() const { return m_value; }
+	
+	@property void domain(string value) { m_domain = value; set(); }
+	@property string domain() const { return m_domain; }
+	
+	@property void path(string value) { m_path = value; set(); }
+	@property string path() const { return m_path; }
+	
+	@property void expires(string value) { m_expires = value; set(); }
+	@property string expires() const { return m_expires; }
+	
+	@property void maxAge(long value) { m_maxAge = value; set(); }
+	@property long maxAge() const { return m_maxAge; }
+
+	void set() { reqresp_.response_setCookie(m_name, m_value, m_path, m_maxAge, m_expires, m_domain); }
+}
+
+final struct DakkaSession {
+	private {
+		string id_;
+		HTTPReqResp reqresp_;
+	}
+
+	this(HTTPReqResp reqresp, string path, size_t options) {
+		reqresp_ = reqresp;
+		reqresp.response_startSession(path, options);
+		id_ =  reqresp.session_id;
+	}
+
+	bool opCast() { return !reqresp_.session_isnull; }
+	@property string id() const { return id_; }
+	bool isKeySet(string key) { return reqresp_.session_isKeySet(key); }
+
+	T get(T)(string key, lazy T def_value = T.init) {
+		import std.conv : to;
+
+		if (isKeySet(key))
+			static if (is(T == string))
+				return reqresp_.session_get(key);
+			else
+				return to!T(reqresp_.session_get(key));
+		else
+			return def_value;
+	}
+
+	void set(T)(string key, T value) {
+		import std.conv : to;
+		static if (is(T == string))
+			reqresp_.session_set(key, value);
+		else
+			reqresp_.session_set(key, to!string(value));
+	}
+
+	int opApply(int delegate(string key, string value) del) {
+		foreach(key; reqresp_.session_keys)
+			if( auto ret = del(key, reqresp_.session_get(key)) != 0 )
+				return ret;
+		return 0;
+	}
+
+	string opIndex(string name) { return reqresp_.session_get(name); }
+	void opIndexAssign(string value, string name) { set(name, value); }
+}
+
+class DakkaHTTPRequest : HTTPRequest {
+	HTTPServerRequest requestImpl;
+	alias requestImpl this;
+
+	this(HTTPReqResp reqresp) {
+		auto data = reqresp.request;
+		requestImpl = new HTTPServerRequest(data.timeCreated, data.port);
+
+		mixin(settingFromType!(RequestData, "data")(11));
+		foreach(k, v; data.headers) {
+			headers[k] = v;
+			requestImpl.headers[k] = v;
+		}
+		foreach(k, v; data.query) {
+			requestImpl.query[k] = v;
+		}
+		foreach(k, v; data.form) {
+			requestImpl.form[k] = v;
+		}
+	}
+}
+
+private {
+	pure string settingFromType(T, string name)(size_t max = size_t.max) {
+		enum T t = T.init;
+		string ret;
+		foreach(i, id; __traits(allMembers, T)) {
+			if (id != "opAssign" && i < max)
+				ret ~= "static if (__traits(hasMember, typeof(this), \""~ id ~ "\"))" ~ id ~ " = " ~ name ~ "." ~ id ~ ";";
+		}
+		return ret;
+	}
+}
---
+++ source/vibe-d_wrappers/dakka/vibe/server.d
@@ -0,0 +1,206 @@
+﻿module dakka.vibe.server;
+import dakka.base.defs;
+import vibe.http.server : HTTPServerRequest, HTTPServerResponse, SessionOption;
+import vibe.http.common : HTTPVersion, HTTPMethod, CookieValueMap;
+import vibe.http.session : Session;
+import vibe.http.status : HTTPStatus;
+import vibe.inet.webform : FormFields;
+import vibe.inet.message : InetHeaderMap;
+import vibe.data.json : Json;
+import std.datetime : SysTime;
+
+final struct RequestData {
+	//HTTPRequest
+	HTTPVersion httpVersion;
+	HTTPMethod method;
+	string requestURL;
+
+	//HTTPServerRequest
+	string peer;
+	bool ssl;
+	string path;
+	string username;
+	string password;
+	string queryString;
+	CookieValueMap cookies;
+	string[string] params;
+
+	// not included by arg copy
+
+	//HTTPRequest
+	string[string] headers;
+
+	//HTTPServerRequest
+	string[string] query;
+	string[string] form;
+
+	ushort port;
+	SysTime timeCreated;
+}
+
+class HTTPReqResp : Actor {
+	import cerealed.attrs;
+
+	@NoCereal private __gshared {
+		HTTPServerRequest request_;
+		HTTPServerResponse response_;
+		Session session_;
+		bool reassigned;
+	}
+
+	@DakkaLocalOnly
+	void assignData(HTTPServerRequest request, HTTPServerResponse response) {
+		synchronized {
+			reassigned = true;
+			request_ = request;
+			response_ = response;
+			session_ = request.session;
+		}
+	}
+
+	@NoCereal {
+		@property {
+			bool hasBeenReassigned() {
+				synchronized {
+					if (reassigned) {
+						reassigned = false;
+						return true;
+					} else {
+						return false;
+					}
+				}
+			}
+
+			/*
+			 * Request 
+			 */
+
+			RequestData request() {
+				import vibe.utils.array : FixedAppender;
+
+				synchronized {
+					string[string] headers;
+					foreach(key, value; request_.headers)
+						headers[key] = value;
+					string[string] query;
+					foreach(key, value; request_.query)
+						headers[key] = value;
+					string[string] form;
+					foreach(key, value; request_.form)
+						headers[key] = value;
+					ushort port = *cast(ushort*)(&request_)[SysTime.sizeof + FixedAppender!(string, 31).sizeof]; // workaround
+					return mixin("RequestData(" ~ argsFromNames!(RequestData, "request_")(11) ~ ", headers, query, form, port, request_.timeCreated)");
+				}
+			}
+
+			@DakkaLocalOnly {
+				import dakka.vibe.client : DakkaHTTPRequest, DakkaHTTPResponse;
+				DakkaHTTPRequest client_request() { return new DakkaHTTPRequest(this); }
+				DakkaHTTPResponse client_response() { return new DakkaHTTPResponse(this); }
+			}
+		}
+
+		/*
+		 * Response
+		 */
+
+		void response_writeBody(ubyte[] data, string content_type = null) {
+			synchronized
+				response_.writeBody(data, content_type);
+		}
+
+		void response_writeVoidBody() {
+			synchronized
+				response_.writeVoidBody();
+		}
+
+		void response_redirect(string url, int status = HTTPStatus.Found) {
+			synchronized
+				response_.redirect(url, status);
+		}
+
+		void response_setCookie(string name, string value, string path = "/", long maxAge = long.min, string expires = null, string domain=null) {
+			synchronized {
+				auto cookie = response_.setCookie(name, value, path);
+				if (maxAge != long.min)
+					cookie.maxAge = maxAge;
+				if (expires !is null)
+					cookie.expires = expires;
+				if (domain !is null)
+					cookie.domain = domain;
+			}
+
+		}
+
+		void response_startSession(string path = "/", size_t options = SessionOption.httpOnly) {
+			synchronized
+				session_ = response_.startSession(path, cast(SessionOption)options);
+		}
+
+		void response_terminateSession() {
+			synchronized
+				response_.terminateSession();
+		}
+
+		/*
+		 * Session
+		 */
+
+		bool session_isnull() {
+			synchronized
+				return session_.id is null;
+		}
+
+		string session_id() {
+			synchronized {
+				assert(session_.id !is null, "Session is currently null. Cannot get id from it.");
+				return session_.id;
+			}
+		}
+
+		bool session_isKeySet(string name) {
+			synchronized {
+				assert(session_.id !is null, "Session is currently null. Cannot get id from it.");
+				return session_.isKeySet(name);
+			}
+		}
+
+		void session_set(string key, string value) {
+			synchronized {
+				assert(session_.id !is null, "Session is currently null. Cannot get id from it.");
+				session_.set(key, value);
+			}
+		}
+
+		string session_get(string key) {
+			synchronized	{
+				assert(session_.id !is null, "Session is currently null. Cannot get id from it.");
+				return session_.get!string(key);
+			}
+		}
+
+		string[] session_keys() {
+			synchronized {
+				assert(session_.id !is null, "Session is currently null. Cannot get id from it.");
+				string[] ret;
+				foreach(k, v; session_) {
+					ret ~= k;
+				}
+				return ret;
+			}
+		}
+	}
+}
+
+private {
+	pure string argsFromNames(T, string name)(size_t max = size_t.max) {
+		enum T t = T.init;
+		string ret;
+		foreach(i, id; __traits(allMembers, T)) {
+			if (id != "opAssign" && i < max)
+				ret ~= name ~ "." ~ id ~ ",";
+		}
+		ret.length--;
+		return ret;
+	}
+}
